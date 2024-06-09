bd871658aad13bdb6e1036040c38c709514710a3
Tim Meeuwsen <tim@doubleweb.nl>
2024-06-09 15:44:20 +0200

    teksten verbetert

--- js/main.js
+++ js/main.js
@@ -31,16 +31,3 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
     });
   });
   
-
-  const imagesContainer = document.querySelector('.slider .images');
-const images = imagesContainer.querySelectorAll('picture');
-const duplicates = [];
-
-images.forEach(image => {
-    const duplicate = image.cloneNode(true);
-    duplicates.push(duplicate);
-});
-
-duplicates.forEach(duplicate => {
-    imagesContainer.appendChild(duplicate);
-});
